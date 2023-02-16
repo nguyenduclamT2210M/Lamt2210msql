c72d466d244a7600c9cbf2b1838ecb9492f7ed79
nguyenduclam <nguyenduclam048@gmail.com>
2023-02-16 22:42:36 +0700

    Create roductslam.sql
    11

---
+++ roductslam.sql
@@ -0,0 +1,191 @@
+insert into group5_products(name, category_id, vendor_id, current_price, bid_price, buy_now_price, end_time, description, additional_info, thumbnail, img1, img2, img3, img4, img5)
+		values(
+			'Amazfit GTR 4',
+			'watch",
+			'samsung',
+			180,
+			210,
+			230,
+			'2022-12-1 13:00',
+			'Amazfit GTR 4 has more than 10 operable modes, advanced workout features and a few new features through the Zepp app. The watch features an enhanced running mode, a new Golf Swing feature, and the ability to connect to the Adidas Running app. The manufacturers are also planning to work together to support Strava through an update. Users will soon be able to download running tracks from the Zepp app onto the watch and see real-time directions.',
+			'color: black, status:99%',
+			'assets/images/vuong',
+			'assets/images/amazfit-gtr-4sili cone-1',
+			'assets/images/amazfit-gtr-4sili cone-2',
+			'assets/images/amazfit-gtr-4sili cone-3',
+			'assets/images/amazfit-gtr-4sili cone-4',
+			'assets/images/amazfit-gtr-4sili cone-5'	
+		)
+		(
+			'Amazfit GTS 2',
+			'watch",
+			'apple',
+			65,
+			75,
+			80,
+			'2022-12-3 20:00',
+			'PAI health assessment systemCalculate calories burned 24-hour heart rate monitoringDifferent types of exerciseStress monitoringSteps Counting Oxygen (SpO2)Sleep monitoringMenstrual cycle tracking',
+			'color: black, status:100%',
+			'assets/images/amazfitfts2',
+			'assets/images/amazfit-gts-2-mini ni-den-1-org',
+			'assets/images/amazfit-gts-2-mini ni-den-2-org',
+            'assets/images/amazfit-gts-2-mini ni-den-3-org',
+			'assets/images/amazfit-gts-2-mini ni-den-4-org',
+			'assets/images/amazfit-gts-2-mini ni-den-5-org'		
+		)
+		(
+			'Befit B4',
+			'watch",
+			'apple',
+			70,
+			85,
+			100,
+			'2022-12-5 23:30',
+			'Blood pressure measurement Exercise mode Skin temperature 24-hour heart rate monitor Sedentary reminder Step count Oxygen monitoring (SpO2) Sleep monitoring',
+			'color: black, status:85%',
+			'assets/images/befib4',
+			'assets/images/befit-b4-black-1',
+			'assets/images/befit-b4-black-2',
+            'assets/images/befit-b4-black-3',
+			'assets/images/befit-b4-black-4',
+			'assets/images/befit-b4-black-5'		
+		)
+		(
+			'Beu active',
+			'watch",
+			'apple',
+			50,
+			60,
+			65,
+			'2022-10-11 11:30',
+			'Measure your heart rateCalculate calories burnedMeasurement of blood pressure24Hour heart rate monitoringSedentary reminderCounting stepsSleep trackingMenstrual cycle tracking',
+			'color: black, status:100%',
+			'assets/images/beuactive1',
+			'assets/images/beu-active-1-3-1',
+			'assets/images/beu-active-1-4-1',
+            'assets/images/beu-active-1-5-1',
+			'assets/images/beu-active-1-6-1',
+			'assets/images/beu-active-1-7-1'		
+		)
+		(
+			'Samsung Galaxy Watch 4',
+			'watch",
+			'samsung',
+			200,
+			250,
+			300,
+            'Measure blood pressure (only support when connected to Samsung phone)Electrocardiogram (support only when connected to Samsung phone)Measurement of heart rateCalculate calories burned Count steps Measure oxygen levels (SpO2)Sleep monitoring',
+			'color: black, status:100%',
+			'assets/images/samsunggalaxywatch4',
+			'assets/images/samsung-galaxy-watch-4-44mm-b-ac-1',
+			'assets/images/samsung-galaxy-watch-4-44mm-b-ac-2',
+            'assets/images/samsung-galaxy-watch-4-44mm-b-ac-3',
+			'assets/images/samsung-galaxy-watch-4-44mm-b-ac-4',
+			'assets/images/samsung-galaxy-watch-4-44mm-b-ac5'		
+		)
+		
+		(
+			'Samsung Galxy Watch 5  ',
+			'watch",
+			'samsung',
+			150,
+			200,
+			250,
+            'Blood pressure measurement (only supports when connected to Samsung phone)Electrocardiogram (supports only when connected to Samsung phone)Measurement of heart rateCalculate calories burned Exercise modeStress monitoringStep count Measure oxygen level (SpO2)Sleep tracking',
+			'color: black, status:85%',
+			'assets/images/68525-Samsung-Galaxy-Watch-5-',
+			'assets/images/samsung-galaxy-watch-5-40-mm-bac-2',
+			'assets/images/samsung-galaxy-watch-5-40-mm-bac-3',
+            'assets/images/samsung-galaxy-watch-5-40-mm-bac-4',
+			'assets/images/samsung-galaxy-watch-5-40-mm-bac-5',
+			'assets/images/samsung-galaxy-watch-5-40-mm-bac-6'		
+		)
+		(
+			'Smart Casper 4k',
+			'tv-audio",
+			'samsung',
+			500,
+		    600,
+			750,
+            'Ambient ModeBrightness/Color Detection Smooth motion Motion Xcelerator TurboReduce gaming lag Auto Low Latency Mode (ALLM)HDR10+Quantum DotQuantum HDRSuper Ultra Wide Game View & Game BarSupreme UHD Dimming',
+			'color: black, status:100%',
+			'assets/images/smart4k',
+			'assets/images/smart-casper-4k-55-inch-55ug610-2',
+			'assets/images/smart-casper-4k-55-inch-55ug610-3',
+            'assets/images/smart-casper-4k-55-inch-55ug610-4',
+			'assets/images/smart-casper-4k-55-inch-55ug610-5',
+			'assets/images/smart-casper-4k-55-inch-55ug610-6'		
+		)
+		(
+			'Google TCL 4K',
+			'tv-audio',
+			'samsung',
+			600,
+			651,
+			1000,
+			'2023-12-04 17:00',
+			'Wide Color GamutGame ModeHDR10HLGSmart HDR',
+			'color: black, status: 100% ',
+			'assets/images/ggtcl4k',
+			'assets/images/google-tcl-4k-55-inch-55p635-1',
+			'assets/images/google-tcl-4k-55-inch-55p635-2',
+			'assets/images/google-tcl-4k-55-inch-55p635-3',
+			'assets/images/google-tcl-4k-55-inch-55p635-4',
+			'assets/images/google-tcl-4k-55-inch-55p635-5'		
+		)
+		(
+			'Smart LG 4K',
+			'tv_audio',
+			'lg',
+			590,
+			651,
+			1000,
+			'2023-10-10 19:00',
+			'Active HDRHGiG Game ModeContent-appropriate Picture ModeFilmMaker ModeReduce gaming lag Auto Low Latency Mode (ALLM)HDR Dynamic Tone MappingHDR10 ProHLG Upscaling to 4K AI UpscalingVideo decoder compatibility (VP9, AV1)HEVC compatible',
+			'color: black, status: 95% ',
+			'assets/images/lg4k',
+			'assets/images/smart-lg-4k-65-inch-65uq7550psf-01',
+			'assets/images/smart-lg-4k-65-inch-65uq7550psf-02',
+			'assets/images/smart-lg-4k-65-inch-65uq7550psf-03',
+			'assets/images/smart-lg-4k-65-inch-65uq7550psf-04',
+			'assets/images/smart-lg-4k-65-inch-65uq7550psf-05'		
+		)
+		(
+			'Android Sony 4K',
+			'tv-audio',
+			'sony',
+			550,
+			651,
+			1000,
+			'2023-10-04 15:00',
+			'Auto Mode Smooth Motion Motionflow XR 200Dolby VisionHDR10HLG Backlight Control Direct LED Frame Dimming 4K X-Reality PRO Object-Based HDR Remaster Color Enhancement Triluminos Pro',
+			'color : black , status: 99%',
+			'assets/images/androi4k',
+			'assets/images/android-sony-4k-55-inch-kd-55x80j-s-1',
+			'assets/images/android-sony-4k-55-inch-kd-55x80j-s-2',
+			'assets/images/android-sony-4k-55-inch-kd-55x80j-s-3',
+			'assets/images/android-sony-4k-55-inch-kd-55x80j-s-4',
+			'assets/images/android-sony-4k-55-inch-kd-55x80j-s-5'		
+		)
+		(
+			'Samsung Led 4k',
+			'tv-audio',
+			'samsung',
+			630,
+			680,
+			800,
+			'2023-04-12 08:00',
+			'Ambient Mode Smooth motion Motion Xcelerator Smooth image movements Auto Motion PlusDynamic Crystal ColorReduce gaming lag Auto Low Latency Mode (ALLM)HDR10+UHD Dimming Backlight Control Contrast Enhancer',
+			'color: black, status: 99%',
+			'assets/images/led4k',
+			'assets/images/led-4k-samsung-ua55au8100-1',
+			'assets/images/led-4k-samsung-ua55au8100-2',
+			'assets/images/led-4k-samsung-ua55au8100-3',
+			'assets/images/led-4k-samsung-ua55au8100-4',
+			'assets/images/led-4k-samsung-ua55au8100-5'		
+		)
+
+
+
+		
+		;
